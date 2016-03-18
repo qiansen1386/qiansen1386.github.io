9ebec0ed8c7ed45ce82f9fe74ad745f5d79b98b2
Paris qian sen <qiansen1386@gmail.com>
2016-03-18 03:17:15 +0000

    alter wercker.config to exclude drafts during deploy but include drafts for testing builds.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-hugo --cleanDestinationDir -D -v --disableSitemap --uglyURLs=true --canonifyURLs=false -t=hyde-x  --baseURL="https://preview.c9users.io/qiansen1386/hugo/gh-pages/public/"
+hugo --cleanDestinationDir -D -v --disableSitemap --buildDrafts=true --uglyURLs=true --canonifyURLs=false -t=hyde-x  --baseURL="https://preview.c9users.io/qiansen1386/hugo/gh-pages/public/"
