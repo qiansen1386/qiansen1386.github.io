b13efbfda9af222834aa18062e92699d44fa0277
Paris qian sen <qiansen1386@gmail.com>
2017-01-23 15:42:42 +0800

    update theme

--- themes/placid/static/js/canvas-nest.js
+++ themes/placid/static/js/canvas-nest.js
@@ -17,7 +17,8 @@
 			z: get_attribute(script, "zIndex", -1), //z-index
 			o: get_attribute(script, "opacity", 0.5), //opacity
 			c: get_attribute(script, "color", "0,0,0"), //color
-			n: get_attribute(script, "count", 99) //count
+			n: get_attribute(script, "count", 99), //count
+			t: get_attribute(script, "trackmouse", 0) //count
 		};
 	}
 	//设置canvas的高宽
@@ -78,14 +79,15 @@
 	the_canvas.style.cssText = "position:fixed;top:0;left:0;z-index:" + config.z + ";opacity:" + config.o;
 	get_by_tagname("body")[0].appendChild(the_canvas);
 	//初始化画布大小
-
 	set_canvas_size(), window.onresize = set_canvas_size;
 	//当时鼠标位置存储，离开的时候，释放当前位置信息
-	window.onmousemove = function(e) {
-		e = e || window.event, current_point.x = e.clientX, current_point.y = e.clientY;
-	}, window.onmouseout = function() {
-		current_point.x = null, current_point.y = null;
-	};
+	if(config.t) {
+		window.onmousemove = function(e) {
+			e = e || window.event, current_point.x = e.clientX, current_point.y = e.clientY;
+		}, window.onmouseout = function() {
+			current_point.x = null, current_point.y = null;
+		};
+	}
 	//随机生成config.n条线位置信息
 	for (var random_lines = [], i = 0; config.n > i; i++) {
 		var x = random() * canvas_width, //随机位置
